fc35b267fbdf105b3336b6cb5b55ea6db1eb7b09
Lucas Zambon <lucas.shidori@gmail.com>
2018-11-02 17:49:42 -0300

    adding logout view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm';
 import firebase from 'firebase';
 
 class App extends Component{
+    
+    state ={
+        isLogged: null
+    }
 
     componentWillMount(){
 
@@ -17,13 +21,47 @@ class App extends Component{
                     storageBucket: 'auth-6ff54.appspot.com',
                     messagingSenderId: '1001660354663'
                 });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if(user){
+                this.setState({isLogged : true});
+                
+            }else{
+                this.setState({isLogged : false});
+                
+            }
+        });
+    }
+
+    renderContent(){
+        switch(this.state.isLogged){
+            
+            case true:
+                return(
+                    <CardSection>
+                    <Button
+                        onClick = { () => firebase.auth().signOut() }
+                        label = {'Logout'}/> 
+                    </CardSection>
+                );
+
+            case false:
+                return <LoginForm/>;
+
+            default:
+                return( 
+                    <CardSection>
+                        <Spinner/>
+                    </CardSection>
+                );
+        }
     }
 
     render(){
         return(
             <View>
                 <Header headerText = {'Authentication'}/>
-                <LoginForm/>
+                {this.renderContent()}
             </View>
         );
     }
